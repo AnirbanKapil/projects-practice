0b468b2f649d0f5b8141d12f5e3f5a2da77b77ea
Anirban Kapil <anirbankapil@gmail.com>
2024-06-09 19:17:41 +0530

    prototype

--- ToDo App/src/components/Input.jsx
+++ ToDo App/src/components/Input.jsx
@@ -2,8 +2,24 @@
 import React, { useState } from 'react'
 
 function Input() {
+  
+  
+  const [task,setTask] = useState("");
+  const [todo,setTodo] = useState([]);
+  
 
-  const [list,setList] = useState("")
+  function handleEvent (event) {
+     setTask(event.target.value)
+      
+  }
+  
+  function handleButton () {
+       setTodo((preV)=>{
+          return [...preV , task]
+       });
+       setTask("")
+  }
+  
 
   return (
     <>
@@ -11,11 +27,20 @@ function Input() {
     <div className="flex flex-wrap justify-center">
         <div className= "">
       <label>
-        <input className='w-[600px]' value={list} placeholder='Task' type='text' />
+        <input className='w-[600px] rounded-lg text-black' value={task} placeholder='Task' type='text' 
+        onChange={handleEvent} />
       </label>
         </div>
-      <button className='text-white bg-blue-600 px-4 mx-4'>Add</button>
+      <button className='text-white bg-blue-600 px-4 mx-4  rounded-lg' onClick={handleButton}>
+        Add</button>
       </div>  
+    </div>
+    <div className='flex justify-center'>
+      <div className='w-2/3 bg-gray-300 mt-10 h-screen text-center'>
+        <ul>
+          {todo.map((item)=><li key={item}>{item}</li>)}
+        </ul>
+      </div>
     </div>
     </>
   )
