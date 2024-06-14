61b07af2283cf57b21774efbb33d258f52774e66
Anirban Kapil <anirbankapil@gmail.com>
2024-06-14 17:25:03 +0530

    half way through

--- social-media/src/components/Post.jsx
+++ social-media/src/components/Post.jsx
@@ -1,15 +1,24 @@
 
 import React from 'react'
 
-function Post() {
+import { RiDeleteBin5Line } from "react-icons/ri";
+
+function Post({post}) {
   return (
-    <div class="card" style={{width: "18rem"}}>
+    <div className="card post-card">
   
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
+  <div className="card-body">
+    <h5 className="card-title">{post.title}</h5>
+    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    <RiDeleteBin5Line />
+    
+  </span>
+    <p className="card-text">{post.body}</p>
+    {post.tags.map((tag)=>(<span className="badge text-bg-primary hastag">{tag}</span>))}
+    <div className="alert alert-dark reactions" role="alert">
+  This post have been reacted by {post.reactions} people
+</div>
+    </div>
 </div>
   )
 }
---
+++ social-media/src/store/post-list-store.jsx
@@ -0,0 +1,49 @@
+import { createContext, useReducer } from "react"
+
+
+export const PostList = createContext({
+   postList : [],
+   addPost : () => {},
+   deletePost : () => {}
+});
+
+const postListReducer = (currPostList,action) => {
+   return currPostList
+}
+
+const PostListProvider = ({children}) => {
+   const [postList,dispatchPostList] = useReducer(postListReducer,DEFAULT_POST_LIST)
+   const addPost = () => {}
+   const deletePost = () => {};
+
+   
+
+   return (<PostList.Provider value={{
+      postList,
+      addPost,
+      deletePost
+   }}>
+    {children}
+   </PostList.Provider>
+   )
+}
+
+const DEFAULT_POST_LIST = [{
+    id : "1",
+    title : "Hiking",
+    body : "Excited about my upcoming travels & hikes",
+    reactions : 4,
+    userId : "user001",
+    tags : ["hike","travel"]
+},
+ {
+   id : "2",
+   title : "A new Car",
+   body : "Bought a new car for myself (Range rover)",
+   reactions : 6,
+   userId : "user005",
+   tags : ["car","rover"]
+ }
+]
+
+export default PostListProvider
